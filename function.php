102fd15c99b478313e278dee5c3f942c72ef5f41
prabhuikiu7i <160728123+prabhuikiu7i@users.noreply.github.com>
2024-11-22 17:44:35 +0530

    Update function.php
    Update a code for get  the shopify token

--- function.php
+++ function.php
@@ -1,9 +1,16 @@
 <?php
 
 class  ShopifyProduct{
-	private $api_url = 'https://mr-motorcycles-nz.myshopify.com/admin/api/2024-10/';
-	
-	private $shopify_token = file_get_contents('shopify_access_token.txt');
+private $api_url = 'https://mr-motorcycles-nz.myshopify.com/admin/api/2024-10/';
+private $shopify_token;
+
+public function __construct() {
+  $this->shopify_token = file_get_contents(__DIR__ . '\shopify_access_token.txt');
+}
+
+public function getShopifyToken() {
+   return $this->shopify_token;
+}
 
 function getProductBySKU($product_sku){
 		
