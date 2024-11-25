1293281922251db4699aa5d2cef448e2d04d6f3b
prabhuikiu7i <prabhuvenkatesh8561@gmail.com>
2024-11-25 17:28:21 +0530

    Update code in testing.php file

--- testing.php
+++ testing.php
@@ -37,6 +37,7 @@ $endCount = $startCount + $batchSize;
 $batchToProcess = array_slice($array_data, $startCount, $batchSize);
 
 	$counter = 0;
+	$productPriceResponse = 0;
 	foreach($batchToProcess as $product){
 		$product_title = $product['ItemName'];
 		$product_sku = $product['SKU'];
